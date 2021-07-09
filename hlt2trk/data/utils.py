149aa11d7c156b92afc07e5b1b4343fd51d04f64
Ouail <28654657+okitouni@users.noreply.github.com>
2021-07-09 12:09:52 -0400

    documentation, formatting

--- hlt2trk/data/utils.py
+++ hlt2trk/data/utils.py
@@ -1,4 +1,16 @@
+import pandas as pd
+from typing import Iterable
+import numpy as np
 
 
-def to_np(x, features):
+def to_np(x: pd.DataFrame, features: Iterable) -> np.ndarray:
+    """Convert dataframe to numpy array given a subset of the columns.
+
+    Args:
+        x (pd.DataFrame): Dataframe to convert.
+        features (Iterable): Iterable of columns to keep.
+
+    Returns:
+        np.ndarray: Array of columns to keep.
+    """
     return x[features].values
