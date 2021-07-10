2000283ca18036c68f89e2cb406fd86851292469
nnolte <niklas.nolte@cern.ch>
2021-07-10 00:01:42 -0400

    add bdt training

--- hlt2trk/models/models.py
+++ hlt2trk/models/models.py
@@ -7,57 +7,60 @@ from hlt2trk.utils import config
 from InfinityNorm import infnorm
 from InfinityNorm import SigmaNet
 
+import lightgbm as lgb
 
-def get_model(cfg):
+
+def _build_module(
+    in_features: int = 1,
+    nclasses: int = 1,
+    nunits: Union[int, Iterable] = 15,
+    nlayers: int = 3,
+    norm: bool = True,
+    activation: Callable = nn.LeakyReLU(),
+    out_activation: Callable = None,
+) -> nn.Module:
+    """
+
+  Parameters
+  ----------
+  in_features : int
+  nclasses : int
+  nunits : [int, Iterable]
+      If int is provided, use nlayers to specify depth. Otherwise,
+      if Iterable, the depth is inferred.
+  nlayers : int
+      Used to specify depth when nunits is int. Ignored if nunits is
+      Iterable.
+  norm : bool
+      Whether or not to use Inf norm. If False a simple nn.Linear is used.
+  activation : Callable
+      Activation used between hidden layers.
+  out_activation : Callable
+      Activation used in the last layer.
+  Returns
+  -------
+  nn.Module
+      Sequential module.
+  """
+    norm_func = infnorm if norm else lambda x: x
+    nunits = [nunits] * (nlayers - 1) if isinstance(nunits, int) else nunits
+    try:
+        nunits = iter(nunits)
+    except TypeError:
+        raise TypeError("nunits must be either int or iterable.")
+    layers = []
+    for out_features in nunits:
+        layers.append(norm_func(nn.Linear(in_features, out_features)))
+        layers.append(activation)
+        in_features = out_features
+    layers.append(norm_func(nn.Linear(in_features, nclasses)))
+    if out_activation is not None:
+        layers.append(out_activation)
+    return nn.Sequential(*layers)
+
+
+def get_model(cfg: config.Configuration) -> Union[nn.Module, lgb.Booster]:
     nfeatures = len(cfg.features)
-
-    def build_module(
-        in_features: int = 1,
-        nclasses: int = 1,
-        nunits: Union[int, Iterable] = 15,
-        nlayers: int = 3,
-        norm: bool = True,
-        activation: Callable = nn.LeakyReLU(),
-        out_activation: Callable = None,
-    ) -> nn.Module:
-        """
-
-      Parameters
-      ----------
-      in_features : int
-      nclasses : int
-      nunits : [int, Iterable]
-          If int is provided, use nlayers to specify depth. Otherwise,
-          if Iterable, the depth is inferred.
-      nlayers : int
-          Used to specify depth when nunits is int. Ignored if nunits is
-          Iterable.
-      norm : bool
-          Whether or not to use Inf norm. If False a simple nn.Linear is used.
-      activation : Callable
-          Activation used between hidden layers.
-      out_activation : Callable
-          Activation used in the last layer.
-      Returns
-      -------
-      nn.Module
-          Sequential module.
-      """
-        norm_func = infnorm if norm else lambda x: x
-        nunits = [nunits] * (nlayers - 1) if isinstance(nunits, int) else nunits
-        try:
-            nunits = iter(nunits)
-        except TypeError:
-            raise TypeError("nunits must be either int or iterable.")
-        layers = []
-        for out_features in nunits:
-            layers.append(norm_func(nn.Linear(in_features, out_features)))
-            layers.append(activation)
-            in_features = out_features
-        layers.append(norm_func(nn.Linear(in_features, nclasses)))
-        if out_activation is not None:
-            layers.append(out_activation)
-        return nn.Sequential(*layers)
 
     if cfg.model == "sigma":
         be_monotonic_in = list(range(len(cfg.features)))
@@ -69,8 +72,8 @@ def get_model(cfg):
 
         sigma_network = nn.Sequential(
             SigmaNet(
-                build_module(in_features=nfeatures, norm=True),
-                sigma=1.5,
+                _build_module(in_features=nfeatures, norm=True),
+                sigma=1.6,
                 monotonic_in=be_monotonic_in,
                 nfeatures=nfeatures,
             ),
@@ -81,13 +84,26 @@ def get_model(cfg):
     elif cfg.model == "regular":
         # regular full dim model for comparison
         regular_model = torch.nn.Sequential(
-            build_module(in_features=nfeatures, norm=False), nn.Sigmoid(),
+            _build_module(in_features=nfeatures, norm=False), nn.Sigmoid(),
         )
         return regular_model
 
+    elif cfg.model == "bdt":
+        clf = lgb.LGBMClassifier(
+            n_estimators=300,
+            learning_rate=0.05,
+            is_unbalance=True,
+            num_leaves=15,
+            boosting_type="gbdt",
+        )
+        return clf
 
-def load_model(cfg):
+
+def load_model(cfg: config.Configuration) -> Union[nn.Module, lgb.Booster]:
     location = config.format_location(config.Locations.model, cfg)
-    m = get_model(cfg)
-    m.load_state_dict(torch.load(location))
+    if cfg.model in ["regular", "sigma"]:
+        m = get_model(cfg)
+        m.load_state_dict(torch.load(location))
+    else:
+        m = lgb.Booster(model_file=location)
     return m
--- scripts/nn/eval_network_on_grid.py
+++ scripts/nn/eval_network_on_grid.py
@@ -2,14 +2,14 @@ import torch
 import numpy as np
 from torch.utils.data import DataLoader, TensorDataset
 from itertools import product
-from hlt2trk.utils.utils import to_np
 from hlt2trk.utils import config
 from hlt2trk.utils.data import get_data
+import lightgbm as lgb
 from hlt2trk.models import load_model
 
 cfg = config.get_config()
 
-sig, bkg = get_data(cfg)
+X, y = get_data(cfg)
 
 nfeats = len(cfg.features)
 
@@ -17,24 +17,37 @@ torch.manual_seed(2)
 
 model = load_model(cfg)
 
-X: torch.Tensor = torch.from_numpy(np.concatenate(
-    [to_np(sig, cfg.features), to_np(bkg, cfg.features)])).float()
-
 limits = [np.quantile(X[:, i], (0.02, 0.98)) for i in range(nfeats)]
 linspaces = [np.linspace(*xi, 100 if nfeats == 2 else 20) for xi in limits]
 grid = np.array(tuple(product(*linspaces)))
-X: torch.Tensor = torch.from_numpy(grid).float()
-data = TensorDataset(X)
-loader = DataLoader(data, batch_size=100000, shuffle=False)
 
-Y: torch.Tensor = torch.zeros(len(X), 1)
-idx = 0
-with torch.no_grad():
-    for (x,) in loader:
-        y: torch.Tensor = model(x)
-        Y[idx: idx + len(y)] = y
-        idx += len(y)
+
+def _eval_torch_network(model: torch.nn.Module, grid: np.ndarray) -> np.ndarray:
+    X: torch.Tensor = torch.from_numpy(grid).float()
+    data = TensorDataset(X)
+    # we might run out of data
+    loader = DataLoader(data, batch_size=100000, shuffle=False)
+    Y: torch.Tensor = torch.zeros(len(X), 1)
+    idx = 0
+    with torch.no_grad():
+        for (x,) in loader:
+            y: torch.Tensor = model(x)
+            Y[idx : idx + len(y)] = y
+            idx += len(y)
+    return Y.numpy()
+
+
+def _eval_bdt(model: lgb.Booster, grid: np.ndarray) -> np.ndarray:
+    return model.predict(grid)
+
+
+if cfg.model == "bdt":
+    eval_fun = _eval_bdt
+elif cfg.model in ["regular", "sigma"]:
+    eval_fun = _eval_torch_network
+
+Y = eval_fun(model, grid).flatten()
 
 # persist the numbers
-torch.save(X, config.format_location(config.Locations.grid_X, cfg))
-torch.save(Y, config.format_location(config.Locations.grid_Y, cfg))
+np.savez(config.format_location(config.Locations.grid_X, cfg), grid)
+np.savez(config.format_location(config.Locations.grid_Y, cfg), Y)
