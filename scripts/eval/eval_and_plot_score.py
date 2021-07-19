ce2dd0ca3f89dc2ce3d9ae74078003db2b95536a
nnolte <niklas.nolte@cern.ch>
2021-07-19 11:22:18 -0400

    on my way to different preselections

--- scripts/eval/eval_and_plot_score.py
+++ scripts/eval/eval_and_plot_score.py
@@ -3,12 +3,13 @@ import matplotlib.pyplot as plt
 import json
 
 from hlt2trk.utils.config import get_config, Locations, format_location
-from hlt2trk.utils.data import get_data, is_signal
+from hlt2trk.utils.data import get_data
 from hlt2trk.models import load_model
 from evaluate import eval_bdt, eval_torch_network, eval_simple
 
 cfg = get_config()
 
+evt_grp = ["eventtype", "EventInSequence"]
 
 def eval(data):
     X = data[cfg.features].to_numpy()
@@ -32,25 +33,34 @@ def roc_auc_score(rates, eff):
 def center(a):
     return (a[1:] + a[:-1]) * 0.5
 
+# TODO this only works for heavy-flavor right now
 
 def plot_rates_vs_effs(data, presel_effs):
-    truths = is_signal(cfg, data["signal_type"])
-    preds = data["pred"]
     cutrange = np.linspace(1, 0, 100)
-    minbias_preds = preds[data.eventtype == 0]
+    tos_preds = data.groupby(evt_grp).tos_pred.agg(max).reset_index()
+    preds = data.groupby(evt_grp).pred.agg(max).reset_index()
+    #truths = (data.groupby(evt_grp).signal_type.agg(max) > 0).reset_index()
+
+    # minimum bias rates (per event)
+    minbias_preds = preds[preds.eventtype == 0].pred.values
     input_rate = 30000  # kHz
     presel_rate = presel_effs[0]
     rates = [input_rate * presel_rate * (minbias_preds > i).mean() for i in cutrange]
 
+
     _, ax = plt.subplots()
     for mode in data.eventtype.unique():
-        pred = preds[data.eventtype == mode]
-        truth = truths[data.eventtype == mode]
-
+        tos_pred = tos_preds[tos_preds.eventtype == mode].tos_pred.values
+        pred = preds[preds.eventtype == mode].pred.values
+        #truth = truths[data.eventtype == mode]
+        
         if mode != 0:
-            eff = [presel_effs[mode] * (pred[truth > 0] > i).mean() for i in cutrange]
+            eff = [presel_effs[mode] * (pred > i).mean() for i in cutrange]
+            tos_eff = [presel_effs[mode] * (tos_pred > i).mean() for i in cutrange]
             auc = roc_auc_score(rates/max(rates), eff)
+            tos_auc = roc_auc_score(rates/max(rates), tos_eff)
             ax.plot(rates, eff, label=f"{mode:^4} / {auc:^5.4f}", c=f"C{mode}")
+            ax.plot(rates, tos_eff, label=f"{mode:^4} / {tos_auc:^5.4f}", ls="--", c=f"C{mode}")
     ax.set_xlabel("rate (kHz)")
     ax.set_ylabel("efficiency")
     #ax.set_ylim(0, max_eff)
@@ -60,16 +70,17 @@ def plot_rates_vs_effs(data, presel_effs):
     ax.legend(loc="best", title="mode / auc")
     plt.savefig(format_location(Locations.rate_vs_eff, cfg))
 
-
 data = get_data(cfg)
 
 data["pred"] = eval(data)
 
 # per event performance evaluation
-grpd = data.groupby(["eventtype", "EventInSequence"]).agg(max).reset_index()
+data = data[evt_grp + ["pred", "signal_type"]]
+data["tos_pred"] = data.pred * (data.signal_type > 0)
+
 
 # get presel_efficiencies
 with open(format_location(Locations.presel_efficiencies, cfg), "r") as f:
     presel_effs = {int(k): v for k, v in json.load(f).items()}
 
-plot_rates_vs_effs(grpd, presel_effs)
+plot_rates_vs_effs(data, presel_effs)
