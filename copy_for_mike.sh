e62dbd497f42f3597e5fdb14a8936aeac4dd89d7
nnolte <niklas.nolte@cern.ch>
2021-08-11 15:19:27 -0400

    fix paths for table

--- copy_for_mike.sh
+++ copy_for_mike.sh
@@ -14,8 +14,8 @@ cp plots/heatmaps/heatmap_nn-inf-oc_minipchi2+sumpt_lhcb_unnormed_heavy-flavor_s
 cp plots/heatmaps/heatmap_nn-inf-oc_minipchi2+sumpt_lhcb_unnormed_heavy-flavor_svPT:1000+trkPT:200+svchi2:20+ipcuttrain:10_max-norm_direct_vector.pdf for_mike/2d/heatmap_nn-inf-oc_noipcut.pdf
 
 #results
-cp results/eff_table_minipchi2+sumpt_lhcb_unnormed_heavy-flavor_svPT:1000+trkPT:200+svchi2:20+ipcuttrain:6_max-norm_direct_vector.txt for_mike/2d/eff_table_withipcut.txt
-cp results/eff_table_minipchi2+sumpt_lhcb_unnormed_heavy-flavor_svPT:1000+trkPT:200+svchi2:20+ipcuttrain:10_max-norm_direct_vector.txt for_mike/2d/eff_table_noipcut.txt
+cp results/latex/eff_table_minipchi2+sumpt_lhcb_unnormed_heavy-flavor_svPT:1000+trkPT:200+svchi2:20+ipcuttrain:6_max-norm_direct_vector.txt for_mike/2d/eff_table_withipcut.txt
+cp results/latex/eff_table_minipchi2+sumpt_lhcb_unnormed_heavy-flavor_svPT:1000+trkPT:200+svchi2:20+ipcuttrain:10_max-norm_direct_vector.txt for_mike/2d/eff_table_noipcut.txt
 
 #4d
 #heatmaps
@@ -30,8 +30,8 @@ cp plots/heatmaps/heatmap_nn-inf-mon-vchi2_fdchi2+sumpt+vchi2+minipchi2_lhcb_unn
 cp plots/heatmaps/heatmap_nn-inf-mon-vchi2_fdchi2+sumpt+vchi2+minipchi2_lhcb_unnormed_heavy-flavor_svPT:1000+trkPT:200+svchi2:20+ipcuttrain:10_max-norm_direct_vector.pdf for_mike/4d/heatmap_nn-inf-mon-vchi2_noipcut.pdf
 
 #results
-cp results/eff_table_fdchi2+sumpt+vchi2+minipchi2_lhcb_unnormed_heavy-flavor_svPT:1000+trkPT:200+svchi2:20+ipcuttrain:6_max-norm_direct_vector.txt for_mike/4d/eff_table_withipcut.txt
-cp results/eff_table_fdchi2+sumpt+vchi2+minipchi2_lhcb_unnormed_heavy-flavor_svPT:1000+trkPT:200+svchi2:20+ipcuttrain:10_max-norm_direct_vector.txt for_mike/4d/eff_table_noipcut.txt
+cp results/latex/eff_table_fdchi2+sumpt+vchi2+minipchi2_lhcb_unnormed_heavy-flavor_svPT:1000+trkPT:200+svchi2:20+ipcuttrain:6_max-norm_direct_vector.txt for_mike/4d/eff_table_withipcut.txt
+cp results/latex/eff_table_fdchi2+sumpt+vchi2+minipchi2_lhcb_unnormed_heavy-flavor_svPT:1000+trkPT:200+svchi2:20+ipcuttrain:10_max-norm_direct_vector.txt for_mike/4d/eff_table_noipcut.txt
 
 #violins
 cp plots/violins/violins_fdchi2+sumpt+vchi2+minipchi2_lhcb_unnormed_heavy-flavor_svPT:1000+trkPT:200+svchi2:20+ipcuttrain:6_max-norm_direct_vector.pdf for_mike/4d/violins_withipcut.pdf
