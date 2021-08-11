129cca329c637b1e0dafa01a5b5fa2bb472cedc9
Ouail <28654657+okitouni@users.noreply.github.com>
2021-08-11 14:55:22 -0400

    changed violin plots

--- scripts/plot/plot_violins.py
+++ scripts/plot/plot_violins.py
@@ -36,25 +36,42 @@ def frac(x):
     return (x - effs["nn-regular"]) / effs["nn-regular"]
 
 
-valid_models = [model for model in models.keys() if model != "nn-regular"]
+submodels = ["bdt", "nn-inf", "nn-one", "nn-inf-oc"]
+valid_models = [model for model in submodels if model in models.keys()]
+# valid_models = [model for model in models.keys() if model != "nn-regular"]
 violins = {model: frac(effs[model]) for model in valid_models}
 
 inds = np.arange(1, len(violins) + 1)
-means = [np.mean(v) for v in violins.values()]
+means = {name: np.mean(v) for name, v in violins.items()}
 
-fig, ax = plt.subplots(1, 1)
-ax.violinplot(violins, vert=False, showextrema=True,
-              showmedians=True, showmeans=False)
 
 with PdfPages(format_location(Locations.violins, cfg)) as pdf:
-    for y, xs in enumerate(violins):
+    fig, ax = plt.subplots(1, 1)
+    ax.violinplot(violins.values(), vert=False, showextrema=True,
+                  showmedians=True, showmeans=False)
+    for y, xs in enumerate(violins.values()):
         ax.scatter(xs, [y + 1] * len(xs))
-    ax.scatter(means, inds, marker='o', color='white', s=30, zorder=3)
+    ax.scatter(means.values(), inds, marker='o', color='white', s=30, zorder=3)
     ax.set_yticks(range(1, len(violins) + 1))
     ax.set_yticklabels(valid_models)
     ax.set_xlabel(
         r"$\frac{\epsilon_{x} - \epsilon_{\mathrm{NN}}}"
         r"{\epsilon_{\mathrm{NN}}}$")
+    pdf.savefig()
+    plt.tight_layout()
+    for name, violin in violins.items():
+        fig, ax = plt.subplots(1, 1)
+        ax.set_title(name)
+        ax.violinplot(violin, vert=False, showextrema=False,
+                      showmedians=True, showmeans=False)
+        ax.scatter(violin, [1] * len(violin))
+        ax.scatter(means[name], 1, marker='o', color='white', s=30, zorder=3)
+        ax.set_yticks([1])
+        ax.set_yticklabels([])
+        xlabel = r"$\frac{\epsilon_{" + name + r"} - \epsilon_{\mathrm{NN}}}" +\
+            r"{\epsilon_{\mathrm{NN}}}$"
+        ax.set_xlabel(xlabel)
+        plt.tight_layout()
+        pdf.savefig()
 
-plt.savefig(format_location(Locations.violins, cfg))
 # plt.savefig(join(dirs.plots, f"candle-plot_{'_'.join(strings)}.pdf"))
